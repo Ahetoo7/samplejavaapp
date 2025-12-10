81bfb0f01dae860c2350807260b435c040079fb9
Ahetoo7 <124400841+Ahetoo7@users.noreply.github.com>
2025-12-11 00:55:43 +0200

    Update PropertyLoader.java

--- src/main/java/com/devopsdemo/utilities/PropertyLoader.java
+++ src/main/java/com/devopsdemo/utilities/PropertyLoader.java
@@ -9,7 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-/**
+/*****
+/*****
  * Utility class for loading properties from files or resource bundles.
  */
 public class PropertyLoader {
